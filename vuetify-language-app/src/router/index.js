c156bc503a454dbc37aead5799a9944aa2461b59
Szabloreki <dawid@surprise.design>
2018-03-13 17:00:18 +0100

    removing un uses file

--- vuetify-language-app/src/router/index.js
+++ vuetify-language-app/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import AddWord from '@/components/modals/AddWord'
 import TableWord from '@/components/TableWords'
 import Trainer from '@/components/trainer/Trainer'
@@ -9,11 +8,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/TableWord',
       name: 'TableWord',
